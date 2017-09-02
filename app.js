6639916ff5217c9af77da9e7e9bdc8a674942ec1
sdickscheid <scott.dickscheid@yahoo.com>
2017-09-02 14:52:18 -0700

    DOM Quest Template - no DOM functionality

---
+++ app.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+let mybtn1 = document.getElementById("1");
+mybtn1.addEventListener("click", function(){
+
+
+})
+
+let mybtn2 = document.getElementById("2");
+mybtn2.addEventListener("click", function(){
+
+
+})
+
+
+let mybtn3 = document.getElementById("3");
+mybtn3.addEventListener("click", function(){
+
+
+})
+
+
+
+})();
